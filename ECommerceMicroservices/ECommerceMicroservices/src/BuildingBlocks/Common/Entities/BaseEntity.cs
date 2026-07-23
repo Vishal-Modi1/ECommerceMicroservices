8737ae102f3a61248c9f604ef2c70999b3b52763
Vishal-Modi1 <modivishalp@gmail.com>
2026-07-23 12:48:27 +0530

    New changes

---
+++ ECommerceMicroservices/ECommerceMicroservices/src/BuildingBlocks/Common/Entities/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.Common.Events;
+
+namespace BuildingBlocks.Common.Entities;
+
+public abstract class BaseEntity
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+
+    public Guid Id { get; protected set; }
+
+    public DateTime CreatedOnUtc { get; protected set; }
+
+    public DateTime? ModifiedOnUtc { get; protected set; }
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
---
+++ ECommerceMicroservices/ECommerceMicroservices/src/BuildingBlocks/Common/Events/IDomainEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Common.Events;
+
+public interface IDomainEvent
+{
+    Guid EventId { get; }
+    DateTime OccurredOnUtc { get; }
+}
